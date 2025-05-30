017074998168aad92529b5028658a637e11657f4
mabrark <muhammad.khan@triosstudent.com>
2025-05-30 10:24:00 -0400

    added a form

--- index.php
+++ index.php
@@ -15,7 +15,37 @@
 <body>
   <?php include("header.php"); ?>
   <main>
-    <h2>Real Estate</h2>
+    <h2>Find Your Ideal Propery</h2>
+    <form action="Save_lead.php" method="post">
+      <label>Name:</label>
+      <input type="text" name="name" required><br>
+
+      <label>Email:</label>
+      <input type="text" name="email" required><br>
+
+      <label>Phone:</label>
+      <input type="text" name="Phone" required><br>
+
+      <label>Location Preference:</label>
+      <input type="text" name="Location" required><br>
+
+      <label>Budget:</label>
+      <input type="text" name="Budget" required><br>
+
+      <select name="Property Type" required>
+         <option value=""> -- Select Type -- </option>
+         <option value="Detached">Detached</option>
+         <option value="Sami-Detached">Sami-Detached</option>
+         <option value="TownHome">TownHome</option>
+         <option value="Banglow">Banglow</option>
+         <option value="Condo">Condo</option>
+         <option value="Commercial">Commercial</option>
+         <option value="Investment">Investment</option>
+         <option value="PreConstruction">PreConstruction</option>
+      </select><br>
+
+      <input type="submit" value="Submit"><br>
+    </form>
 
     <table>
 
